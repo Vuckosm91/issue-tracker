bada686a57a08f7307c413f9c9bc203b7c7bc02a
Vucko <vuckosm91@gmail.com>
2024-09-08 00:47:23 +0200

    Filter issues

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Status } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const IssueStatusFilter = () => {
@@ -11,8 +12,15 @@ const IssueStatusFilter = () => {
     { label: "Closed", value: "CLOSED" },
   ];
 
+  const router = useRouter();
+
   return (
-    <Select.Root>
+    <Select.Root
+      onValueChange={(status) => {
+        const query = status ? `?status=${status}` : "";
+        router.push("/issues/list" + query);
+      }}
+    >
       {/* @ts-ignore */}
       <Select.Trigger placeholder="Filter by status..." />
       <Select.Content>
