246042606c608a50b2d7e27eb2a042d24519a9a5
Vucko <vuckosm91@gmail.com>
2024-09-08 02:49:47 +0200

    Implement pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 import Image from "next/image";
 import Pagination from "./components/Pagination";
 
-export default function Home() {
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
   return (
     <>
-      <Pagination itemCount={100} currentPage={2} pageSize={10} />
+      <Pagination
+        itemCount={100}
+        pageSize={10}
+        currentPage={parseInt(searchParams.page)}
+      />
     </>
   );
 }
